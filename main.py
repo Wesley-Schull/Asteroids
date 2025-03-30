8d7ac02afa415c5017e50db6aac5daf39b43d244
Wes_Schull <varnathin@gmail.com>
2025-03-30 00:07:41 -0400

    We have a blank canvas that opens for our game!

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_is_running = True
+
+    while game_is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(000000)
+        pygame.display.flip()
+
+    
+
+if __name__ == "__main__":
+    main()
