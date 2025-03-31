7ff4bdac24de8c06a248a4128c285490995366d4
Wes_Schull <varnathin@gmail.com>
2025-03-30 23:45:17 -0400

    Got a player drawn on-screen! They can't move, but they _do_ exist!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -8,6 +9,10 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0 # delta_time - the time since the last frame was drawn
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     game_is_running = True
 
@@ -15,8 +20,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
     
 
